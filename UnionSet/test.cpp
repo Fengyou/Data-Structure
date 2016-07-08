0e1e101c8e5a070c0e7fe4e7cc3ef527d37f02ec
Fengyou <862954838@qq.com>
2016-07-08 23:19:51 +0800

    Update test.cpp

--- UnionSet/test.cpp
+++ UnionSet/test.cpp
@@ -4,7 +4,7 @@ void Test()
 {
 	int a[3][2] = { { 1, 2 }, { 2, 3 }, { 4, 5 } };
 	UnionSet us(5);
-	cout << us.CircleOfFriends(5, 3, a) << endl;
+	cout <<"Count:"<< us.CircleOfFriends(5, 3, a) << endl;
 }
 
 int main()
